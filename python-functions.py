6fdcd51b7f933cdffaed4c2d662b5fb97450a5e9
bnagle22 <naglebenjamin@gmail.com>
2021-11-16 14:47:05 -0500

    solve ex4

--- python-functions.py
+++ python-functions.py
@@ -6,7 +6,31 @@ def sum_to(n):
     sum += i
   return sum
 
+# Ex 2
+
+def largest(nums):
+  largest = 0
+  for i in nums:
+    if i > largest:
+      largest = i
+  return largest
+
+# Ex 3
+def occurances(a, b):
+  new_str = a.replace(b, '')
+  count = (len(a) - len(new_str)) / len(b)
+  return int(count)
+
+# Ex 4
+def product(*args):
+  product = 1
+  for i in args:
+    product *= i
+  return product
 
 
 # function calls
 print(sum_to(6))
+print(largest([10, 4, 15, 2]))
+print(occurances("fleeep fleep", 'eee'))
+print(product(3, 4, 2, 10))
