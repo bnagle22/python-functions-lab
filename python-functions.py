9e9d8a0f9437287714014d33a9683def50f955d7
bnagle22 <naglebenjamin@gmail.com>
2021-11-18 09:58:33 -0500

    add solution to ex3

--- python-functions.py
+++ python-functions.py
@@ -16,10 +16,32 @@ def largest(nums):
   return largest
 
 # Ex 3
-def occurances(a, b):
-  new_str = a.replace(b, '')
-  count = (len(a) - len(new_str)) / len(b)
-  return int(count)
+# Works for any length of b:
+# def occurrences(a, b):
+#   new_str = a.replace(b, '')
+#   count = (len(a) - len(new_str)) / len(b)
+#   return int(count)
+
+# Works if b has length 1, 2, or 3:
+def occurrences(a, b):
+  count = 0
+  chars = len(b)
+  if chars == 1:
+    for i in range(len(a)):
+      if (a[i]) == b:
+        count += 1
+  elif chars == 2:
+    for i in range(len(a)-1):
+      if (a[i]+ a[i + 1]) == b:
+        count += 1
+  elif chars == 3:
+    for i in range(len(a)-2):
+      if (a[i] + a[i + 1] + a[i + 2]) == b:
+        count += 1
+
+  return count
+
+# print(occurrences('fleeep floop fleeep', 'fl'))
 
 # Ex 4
 def product(*args):
@@ -30,7 +52,7 @@ def product(*args):
 
 
 # function calls
-print(sum_to(6))
-print(largest([10, 4, 15, 2]))
-print(occurances("fleeep fleep", 'eee'))
-print(product(3, 4, 2, 10))
+# print(sum_to(6))
+# print(largest([10, 4, 15, 2]))
+# print(occurrences("fleeep fleep", 'eee'))
+# print(product(3, 4, 2, 10))
